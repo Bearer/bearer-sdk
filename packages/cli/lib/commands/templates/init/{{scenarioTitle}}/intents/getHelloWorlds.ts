7f4973bcc6bf1436104d76c13563d6adb4244bbf
Tanguy Antoine <antoine.tanguy@apizi.com>
2018-06-20 17:53:19 +0200

    Update templates

---
+++ packages/cli/lib/commands/templates/init/{{scenarioTitle}}/intents/getHelloWorlds.ts
@@ -0,0 +1,18 @@
+const { GetCollection } = require('@bearer/intents')
+
+export default class GetHelloWorlsIntent {
+  static intentName: string = 'getHelloWorlds'
+  static intentType: any = GetCollection
+
+  static action(token: string, params: any, callback: (params: any) => void) {
+    callback({
+      collection: [
+        'hello world',
+        'Bonjour le monde',
+        'Witaj świecie',
+        'hello ao',
+        'こんにちは世界'
+      ]
+    })
+  }
+}
