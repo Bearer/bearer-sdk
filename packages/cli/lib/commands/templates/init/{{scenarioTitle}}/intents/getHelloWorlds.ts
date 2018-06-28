a167806625a4cdd9c2e0561e8ac8892c807faf76
Radek Molenda <radek.molenda@gmail.com>
2018-06-28 14:12:39 +0200

    Better get hello worlds

--- packages/cli/lib/commands/templates/init/{{scenarioTitle}}/intents/getHelloWorlds.ts
+++ packages/cli/lib/commands/templates/init/{{scenarioTitle}}/intents/getHelloWorlds.ts
@@ -1,18 +1,14 @@
 import { GetCollection } from '@bearer/intents'
+import { CLIENT } from './client'
 
 export default class GetHelloWorlsIntent {
   static intentName: string = 'getHelloWorlds'
   static intentType: any = GetCollection
 
   static action(token: string, params: any, callback: (params: any) => void) {
-    callback({
-      collection: [
-        'hello world',
-        'Bonjour le monde',
-        'Witaj świecie',
-        'hello ao',
-        'こんにちは世界'
-      ]
+    CLIENT.get('/people').then(({ data }) => {
+      console.log(data)
+      callback({ collection: data.results })
     })
   }
 }
