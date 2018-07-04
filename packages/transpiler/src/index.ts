3f2f238283352ccc1110f797436778bfff660f04
Tanguy Antoine <antoine.tanguy@apizi.com>
2018-07-04 19:04:53 +0200

    Change module target

--- packages/transpiler/src/index.ts
+++ packages/transpiler/src/index.ts
@@ -8,7 +8,6 @@ export interface TranspilerConfiguration extends ts.CompilerOptions {
   outDir: string
 }
 
-export interface Strategy {}
 export default class Transpiler {
   constructor(private readonly options: TranspilerConfiguration) {}
 
