575207736947d8e0b5c39cc3f291f4f4e6f60775
Radek Molenda <radek.molenda@gmail.com>
2018-07-05 14:42:53 +0200

    Use language service to transpile the files

--- packages/transpiler/bin/bearer-tst.ts
+++ packages/transpiler/bin/bearer-tst.ts
@@ -2,25 +2,40 @@
 
 // import Transpiler from '../src/index'
 import * as ts from 'typescript'
-import * as fs from 'fs'
+import * as fs from 'fs-extra'
+import * as path from 'path'
 
 import ComponentTransformer from '../src/component-transformer'
+
+const compilerOptions = ts.readConfigFile('tsconfig.json', ts.sys.readFile)
+  .config
+let host = ts.createCompilerHost(compilerOptions)
+
+const SCREENS_DIRECTORY = path.join(process.cwd(), 'screens')
+const BUILD_DIRECTORY = path.join(SCREENS_DIRECTORY, '.build')
+fs.emptyDirSync(BUILD_DIRECTORY)
 
 type TransformerOptions = {
   verbose?: true
 }
 
-const resultFile = ts.createSourceFile(
-  'tmp.ts',
-  '',
-  ts.ScriptTarget.Latest,
-  /*setParentNodes*/ false,
-  ts.ScriptKind.TS
-)
-const printer = ts.createPrinter({
-  newLine: ts.NewLineKind.LineFeed
-})
+const sourceCode = tsSourceFile => {
+  const resultFile = ts.createSourceFile(
+    'tmp.ts',
+    '',
+    ts.ScriptTarget.Latest,
+    /*setParentNodes*/ false,
+    ts.ScriptKind.TS
+  )
+  const printer = ts.createPrinter({
+    newLine: ts.NewLineKind.LineFeed
+  })
 
+  return printer.printNode(ts.EmitHint.Unspecified, tsSourceFile, resultFile)
+}
+
+const { config } = ts.readConfigFile('project/tsconfig.json', ts.sys.readFile)
+const parsed = ts.parseJsonConfigFileContent(config, ts.sys, process.cwd())
 function storeOutput({
   verbose
 }: TransformerOptions = {}): ts.TransformerFactory<ts.SourceFile> {
@@ -29,88 +44,23 @@ function storeOutput({
       console.log.apply(this, args)
     }
   }
-  return _transformContext => {
+  return transformContext => {
     return tsSourceFile => {
-      const result = printer.printNode(
-        ts.EmitHint.Unspecified,
-        tsSourceFile,
-        resultFile
-      )
-      log('myFile:', result)
+      const { base, dir } = path.parse(tsSourceFile.fileName)
+      let outPath = tsSourceFile.fileName
+        .replace(SCREENS_DIRECTORY, BUILD_DIRECTORY)
+        .replace(/js$/, 'ts')
+        .replace(/jsx$/, 'tsx')
+
+      fs.ensureFileSync(outPath)
+      fs.writeFileSync(outPath, sourceCode(tsSourceFile))
+
       return tsSourceFile
     }
   }
 }
 
-const formatHost: ts.FormatDiagnosticsHost = {
-  getCanonicalFileName: path => path,
-  getCurrentDirectory: ts.sys.getCurrentDirectory,
-  getNewLine: () => ts.sys.newLine
-}
-
-const configPath = ts.findConfigFile(
-  /*searchPath*/ process.cwd(),
-  ts.sys.fileExists,
-  'tsconfig.json'
-)
-
-if (!configPath) {
-  throw new Error("Could not find a valid 'tsconfig.json'.")
-}
-
-const createProgram = ts.createSemanticDiagnosticsBuilderProgram
-
-const host = ts.createWatchCompilerHost(
-  configPath,
-  {},
-  ts.sys,
-  createProgram,
-  reportDiagnostic,
-  reportWatchStatusChanged
-)
-
-const origCreateProgram = host.createProgram
-
-host.createProgram = (
-  rootNames: ReadonlyArray<string>,
-  options,
-  host,
-  oldProgram
-) => {
-  console.log("** We're about to create the program! **")
-  return origCreateProgram(rootNames, options, host, oldProgram)
-}
-const origPostProgramCreate = host.afterProgramCreate
-
-host.afterProgramCreate = program => {
-  console.log('** We finished making the program! **')
-  origPostProgramCreate!(program)
-}
-
-// `createWatchProgram` creates an initial program, watches files, and updates the program over time.
-// // const tranpspiler = new Transpiler(options, program)
-
-function reportDiagnostic(diagnostic: ts.Diagnostic) {
-  console.error(
-    'Error',
-    diagnostic.code,
-    ':',
-    ts.flattenDiagnosticMessageText(
-      diagnostic.messageText,
-      formatHost.getNewLine()
-    )
-  )
-}
-
-var program = ts.createWatchProgram(host).getProgram()
-
-function reportWatchStatusChanged(diagnostic: ts.Diagnostic) {
-  console.info(ts.formatDiagnostic(diagnostic, formatHost))
-
-  if (program) {
-    program.emit(undefined, undefined, undefined, undefined, transformers)
-  }
-}
+console.log(parsed.fileNames)
 
 const transformers: ts.CustomTransformers = {
   before: [
@@ -119,3 +69,104 @@ const transformers: ts.CustomTransformers = {
   ],
   after: []
 }
+
+function watch(rootFileNames: string[], options: ts.CompilerOptions) {
+  const files: ts.MapLike<{ version: number }> = {}
+
+  // initialize the list of files
+  rootFileNames.forEach(fileName => {
+    files[fileName] = { version: 0 }
+  })
+
+  // Create the language service host to allow the LS to communicate with the host
+  const servicesHost: ts.LanguageServiceHost = {
+    getScriptFileNames: () => rootFileNames,
+    getScriptVersion: fileName =>
+      files[fileName] && files[fileName].version.toString(),
+    getScriptSnapshot: fileName => {
+      if (!fs.existsSync(fileName)) {
+        return undefined
+      }
+
+      return ts.ScriptSnapshot.fromString(fs.readFileSync(fileName).toString())
+    },
+    getCurrentDirectory: () => process.cwd(),
+    getCompilationSettings: () => options,
+    getDefaultLibFileName: options => ts.getDefaultLibFilePath(options),
+    getCustomTransformers: () => transformers,
+    fileExists: ts.sys.fileExists,
+    readFile: ts.sys.readFile,
+    readDirectory: ts.sys.readDirectory
+  }
+
+  // Create the language service files
+  const services = ts.createLanguageService(
+    servicesHost,
+    ts.createDocumentRegistry()
+  )
+
+  // Now let's watch the files
+  rootFileNames.forEach(fileName => {
+    // First time around, emit all files
+    emitFile(fileName)
+
+    // Add a watch on the file to handle next change
+    fs.watchFile(
+      fileName,
+      { persistent: true, interval: 250 },
+      (curr, prev) => {
+        // Check timestamp
+        if (+curr.mtime <= +prev.mtime) {
+          return
+        }
+
+        // Update the version to signal a change in the file
+        files[fileName].version++
+
+        // write the changes to disk
+        emitFile(fileName)
+      }
+    )
+  })
+
+  function emitFile(fileName: string) {
+    let output = services.getEmitOutput(fileName)
+
+    if (!output.emitSkipped) {
+      console.log(`Emitting ${fileName} like a pro`)
+    } else {
+      console.log(`Emitting ${fileName} failed`)
+      logErrors(fileName)
+    }
+  }
+
+  function logErrors(fileName: string) {
+    let allDiagnostics = services
+      .getCompilerOptionsDiagnostics()
+      .concat(services.getSyntacticDiagnostics(fileName))
+      .concat(services.getSemanticDiagnostics(fileName))
+
+    allDiagnostics.forEach(diagnostic => {
+      let message = ts.flattenDiagnosticMessageText(
+        diagnostic.messageText,
+        '\n'
+      )
+      if (diagnostic.file) {
+        let { line, character } = diagnostic.file.getLineAndCharacterOfPosition(
+          diagnostic.start!
+        )
+        console.log(
+          `  Error ${diagnostic.file.fileName} (${line + 1},${character +
+            1}): ${message}`
+        )
+      } else {
+        console.log(`  Error: ${message}`)
+      }
+    })
+  }
+}
+
+// Start the watcher
+watch(parsed.fileNames, {
+  module: ts.ModuleKind.CommonJS
+})
