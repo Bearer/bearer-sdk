b638e712ed6c5388d593ab7901b4541c982270f6
Pierre Repetto-Andipatin <pierre@bearer.sh>
2018-07-25 13:15:53 +0200

    fix: clean build

--- packages/cli/src/lib/commands/startLocalDevelopmentServer/auth.ts
+++ packages/cli/src/lib/commands/startLocalDevelopmentServer/auth.ts
@@ -5,13 +5,13 @@ import * as uuidv1 from 'uuid/v1'
 const router = new Router({ prefix: '/' })
 
 router.get('v1/user/initialize', ctx => {
-  if (!ctx.request.signedCookies || !ctx.request.signedCookies.uuid) {
-    ctx.cookie = {
-      uuid: uuidv1(),
-      maxAge: 2592000000,
-      signed: true
-    } //expires in one month
-  }
+  // if (!ctx.request.signedCookies || !ctx.request.signedCookies.uuid) {
+  //   ctx.cookie = {
+  //     uuid: uuidv1(),
+  //     maxAge: 2592000000,
+  //     signed: true
+  //   } //expires in one month
+  // }
   ctx.body = '<html><head></head><body><script>'
   ctx.body += fs.readFileSync(__dirname + '/iframe.js')
   ctx.body += '</script></body></html>'
