724a69d3e42466f323ec8f3910ed9ac7effe13db
Pierre Repetto-Andipatin <pierre@bearer.sh>
2018-07-27 09:47:59 +0200

    fix: change terminology

--- packages/cli/templates/generate/blankView/{{fileName}}.tsx
+++ packages/cli/templates/generate/blankView/{{fileName}}.tsx
@@ -2,15 +2,15 @@ import { Component } from '@bearer/core'
 
 @Component({
   tag: '{{componentTagName}}',
-  styleUrl: '{{viewName}}.css',
+  styleUrl: '{{componentFileName}}.css',
   shadow: true
 })
-export class {{viewName}} {
+export class {{componentName}} {
   render() {
     return (
       <div class="root">
         <bearer-typography as="h2">
-          {{viewName}} Component
+          {{componentName}} Component
         </bearer-typography>
       </div>
     )
