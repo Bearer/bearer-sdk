76b1f67b61e22d30a5b6f225e8a819f8a71495ee
Tanguy Antoine <antoine@bearer.sh>
2018-08-03 09:57:04 +0200

    fix(core): pass boolean to maybeInitializedSession promise

--- packages/core/src/Bearer.ts
+++ packages/core/src/Bearer.ts
@@ -38,7 +38,7 @@ class Bearer {
 
   private iframe: HTMLIFrameElement
   private isSessionInitialized: boolean = false
-  private allowIntegrationRequests: () => void
+  private allowIntegrationRequests: (initialize: true) => void
   private _maybeInitialized: Promise<boolean>
 
   constructor(args) {
@@ -132,7 +132,7 @@ class Bearer {
   private sessionInitialized(_event) {
     console.log('[BEARER]', 'session initialized')
     this.isSessionInitialized = true
-    this.allowIntegrationRequests()
+    this.allowIntegrationRequests(true)
   }
 
   askAuthorizations({ scenarioId, setupId }) {
