20b2c99078cb50f668e9cff7568c02cbe940d540
Tanguy Antoine <antoine@bearer.sh>
2018-08-10 13:07:56 +0200

    fix(cli): rely on binary

--- packages/cli/src/BaseLegacyCommand.ts
+++ packages/cli/src/BaseLegacyCommand.ts
@@ -1,11 +1,11 @@
 import Command from '@oclif/command'
 import { spawn } from 'child_process'
-import * as path from 'path'
+// import * as path from 'path'
 
 export default abstract class extends Command {
   runLegacy(cmdArgs: any[]) {
-    const cliEntry = path.join(__dirname, '../node_modules/@bearer/bearer-cli/dist/bin/index.js')
+    // const cliEntry = path.join(__dirname, '../node_modules/@bearer/bearer-cli/dist/bin/index.js')
     this.debug('Legacy command arguments', JSON.stringify(cmdArgs))
-    spawn('node', [cliEntry, ...cmdArgs], { stdio: 'inherit' })
+    spawn('_bearer-cli', cmdArgs, { stdio: 'inherit' })
   }
 }
