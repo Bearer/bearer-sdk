521da58d7db035eff9f04c08a2ce95546e2621f0
Radek Molenda <radek.molenda@gmail.com>
2018-08-17 14:12:52 +0200

    feat(cli): generate the spec.ts in bearer new cmd

---
+++ packages/cli/templates/init/noauth/{{scenarioTitle}}/spec.ts
@@ -0,0 +1,22 @@
+export default {
+  components: [
+    {
+      classname: 'FeatureAction',
+      isRoot: true,
+      initialTagName: 'feature-action',
+      group: 'feature'
+    },
+    {
+      classname: 'FeatureDisplay',
+      isRoot: true,
+      initialTagName: 'feature-display',
+      group: 'feature'
+    },
+    {
+      classname: 'SetupAction',
+      isRoot: true,
+      initialTagName: 'setup-action',
+      group: 'setup'
+    }
+  ]
+}
