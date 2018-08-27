fcc059ee51d6d44131f8ca4c3f82833c4e665302
numidiasoft <tarik@bearer.sh>
2018-08-27 13:05:23 +0200

    write the .npmrc to the root dir of the repo

--- .jenkins/prepare.sh
+++ .jenkins/prepare.sh
@@ -1,5 +1,18 @@
 #! /bin/bash
 
 echo "init dependencies"
-echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
-ls -la ~/.npmrc 
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
+ls -la  .
+
+mkdir -p ~/.ssh
+cat $JENKINS_PRIVATE_KEY >> ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+
+git config --global user.email jenkins@bearer.sh
+git config --global user.name   jenkins-br
+
+git_url=$(git config --get remote.origin.url | sed "s/https:\/\/github\.com\//git@github\.com:/")
+git remote set-url origin $git_url
+
+echo $git_url
