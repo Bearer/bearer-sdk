fde2daee70c7d772898c67909a087325b796a364
Tanguy Antoine <tanguyantoine@users.noreply.github.com>
2018-08-31 10:55:40 +0200

    feat(cli): Prepare command (#312)
    * feat(cli): move prepare views to cli
    
    * feat(cli): allow empty flag to prepare view
    
    * feat(cli): prepare intents command
    
    * feat(cli): hide commands

---
+++ packages/cli/src/commands/prepare/views.ts
@@ -0,0 +1,47 @@
+import { flags } from '@oclif/command'
+import * as path from 'path'
+
+import BaseCommand from '../../BaseCommand'
+import { RequireScenarioFolder } from '../../utils/decorators'
+import { copyFiles, ensureFolderExists, ensureSymlinked } from '../../utils/helpers'
+
+export default class PrepareViews extends BaseCommand {
+  static description = 'Prepare scenario views'
+  static hidden = true
+  static flags = {
+    ...BaseCommand.flags,
+    empty: flags.boolean()
+  }
+
+  static args = []
+
+  @RequireScenarioFolder()
+  async run() {
+    const { flags } = this.parse(PrepareViews)
+    // Prepare folder structure
+    this.debug('Preparing views strucutre')
+    ensureFolderExists(this.locator.buildViewsDir, flags.empty)
+    ensureFolderExists(this.locator.buildViewsComponentsDir)
+
+    this.debug('Symlinking node_modules')
+    ensureSymlinked(
+      this.locator.scenarioRootResourcePath('node_modules'),
+      this.locator.buildViewsResourcePath('node_modules')
+    )
+
+    this.debug('Symlinking package.json')
+    ensureSymlinked(
+      this.locator.scenarioRootResourcePath('package.json'),
+      this.locator.buildViewsResourcePath('package.json')
+    )
+
+    this.debug('Copying stencil config')
+    const vars = { componentTagName: this.case.kebab(this.bearerConfig.scenarioConfig.scenarioTitle) }
+    await copyFiles(this, 'start', this.locator.buildViewsDir, vars)
+
+    ensureSymlinked(
+      this.locator.buildViewsResourcePath('global'),
+      path.join(this.locator.buildViewsComponentsDir, 'global')
+    )
+  }
+}
