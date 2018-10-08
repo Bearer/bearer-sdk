0363b5e371f2b10159467e7709d034f709f563a5
Tanguy Antoine <tanguyantoine@users.noreply.github.com>
2018-10-08 12:10:45 +0200

    feat(transpiler): Output component api to manifest (#356)
    * refactor(transpiler): move to functions to the outside
    
    * feat(transpiler): add output api to manifest
    
    * feat(transpiler): gather output meta and drop it
    
    * refactor(transpiler): PR feedbacks

--- packages/transpiler/__test__/__fixtures__/gather-metadata/root-component-with-api.tsx
+++ packages/transpiler/__test__/__fixtures__/gather-metadata/root-component-with-api.tsx
@@ -1,5 +1,15 @@
-import { Prop, RootComponent } from '@bearer/core'
+import { Event, EventEmitter, Prop, RootComponent } from '@bearer/core'
 
+type TPullRequest = {
+  name: string
+  status: 'open' | 'closed'
+}
+
+type TPayloadEvent = {
+  something: TPullRequest
+  somethingElse: string
+  anEnum: 'ok' | 'ko'
+}
 @RootComponent({
   group: 'complex-feature',
   role: 'display'
@@ -9,4 +19,21 @@ export class FeatDisplayRootComponent {
   aStringProp: string = 'ok'
   @Prop()
   aNumberProp: number = 5
+
+  @Event()
+  nonTypedEvent: EventEmitter
+
+  @Event()
+  typedEvent: EventEmitter<{ pullRequest: TPullRequest; aNumber: number; anEnum: 'ok' | 'ko' }>
+
+  @Event()
+  typedEventWithType: EventEmitter<TPayloadEvent>
+  @Event()
+  typedEventWithTypeArray: EventEmitter<string[]>
+  @Event()
+  typedEventWithTypeString: EventEmitter<string>
+  @Event()
+  typedEventWithTypeNumber: EventEmitter<number>
+  @Event()
+  typedEventWithTypeBoolean: EventEmitter<boolean>
 }
--- packages/transpiler/src/transformers/gather-metadata.ts
+++ packages/transpiler/src/transformers/gather-metadata.ts
@@ -1,11 +1,67 @@
 import * as Case from 'case'
 import * as ts from 'typescript'
+import { JsonSchemaGenerator } from 'typescript-json-schema'
 
 import { Component, Decorators } from '../constants'
 import { getDecoratorNamed, getExpressionFromDecorator, hasDecoratorNamed } from '../helpers/decorator-helpers'
-import { TransformerOptions } from '../types'
+import { TComponentInputDefinition, TComponentOutputDefinition, TOuputFormat, TransformerOptions } from '../types'
 
-export default function GatherMetadata({ metadata }: TransformerOptions): ts.TransformerFactory<ts.SourceFile> {
+const UNSPECIFIED = 'unspecified'
+
+export default function GatherMetadata({
+  metadata,
+  generator
+}: TransformerOptions & { generator: JsonSchemaGenerator }): ts.TransformerFactory<ts.SourceFile> {
+  function propAsInput(tsProp: ts.PropertyDeclaration): TComponentInputDefinition {
+    return {
+      name: (tsProp.name as ts.Identifier).escapedText.toString(),
+      type: tsProp.type.kind === ts.SyntaxKind.NumberKeyword ? 'number' : 'string',
+      default: (tsProp.initializer as ts.Expression).getText()
+    }
+  }
+
+  function propInitializerAsJson(tsType: ts.TypeReferenceNode): TOuputFormat {
+    if (!tsType.typeArguments || !Boolean(tsType.typeArguments.length) || !generator) {
+      return UNSPECIFIED
+    }
+    const typeArgument = tsType.typeArguments[0]
+
+    switch (typeArgument.kind) {
+      case ts.SyntaxKind.TypeReference:
+        // if (typeArgument) console.log('[BEARER]', 'typeArgument', typeArgument.getText())
+        return generator.getSchemaForSymbol(typeArgument.getText()) as any
+      case ts.SyntaxKind.TypeLiteral:
+        return { type: 'object' } as any
+      case ts.SyntaxKind.NumberKeyword:
+        return { type: 'number' }
+      case ts.SyntaxKind.BooleanKeyword:
+        return { type: 'boolean' }
+      case ts.SyntaxKind.StringKeyword:
+        return { type: 'string' }
+    }
+    return {}
+  }
+
+  function eventAsOutput(tsProp: ts.PropertyDeclaration): TComponentOutputDefinition {
+    return {
+      name: (tsProp.name as ts.Identifier).escapedText.toString(),
+      payloadFormat: propInitializerAsJson(tsProp.type as ts.TypeReferenceNode)
+    }
+  }
+
+  function collectInputs(tsClass: ts.ClassDeclaration): Array<TComponentInputDefinition> {
+    return tsClass.members
+      .filter(member => ts.isPropertyDeclaration(member) && hasDecoratorNamed(member, Decorators.Prop))
+      .map(propAsInput)
+      .filter(prop => prop.name !== Component.bearerContext)
+  }
+
+  function collectOutputs(tsClass: ts.ClassDeclaration): Array<TComponentOutputDefinition> {
+    return tsClass.members
+      .filter(member => ts.isPropertyDeclaration(member) && hasDecoratorNamed(member, Decorators.Event))
+      .map(eventAsOutput)
+  }
+
   function getTagNames(tagName: string): { initialTagName: string; finalTagName: string } {
     const finalTag =
       metadata.prefix && metadata.suffix
@@ -15,25 +71,6 @@ export default function GatherMetadata({ metadata }: TransformerOptions): ts.Tra
       initialTagName: tagName,
       finalTagName: finalTag
     }
-  }
-
-  function propAsInput(tsProp: ts.PropertyDeclaration) {
-    return {
-      name: (tsProp.name as ts.Identifier).escapedText,
-      type: tsProp.type.kind === ts.SyntaxKind.NumberKeyword ? 'number' : 'string',
-      default: (tsProp.initializer as ts.Expression).getText()
-    }
-  }
-
-  function collectInputs(tsClass: ts.ClassDeclaration): Array<any> {
-    return tsClass.members
-      .filter(member => ts.isPropertyDeclaration(member) && hasDecoratorNamed(member, Decorators.Prop))
-      .map(propAsInput)
-      .filter(prop => prop.name !== Component.bearerContext)
-  }
-
-  function collectOutputs(_tsClass: ts.ClassDeclaration): Array<any> {
-    return []
   }
 
   function visit(node: ts.Node): ts.Node {
