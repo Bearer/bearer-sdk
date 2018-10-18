612e1f657d0129c609c1c349567afe5e5d97d087
Tanguy Antoine <antoine@bearer.sh>
2018-10-18 17:47:23 +0200

    fix(create-bearer): wording

--- packages/create-bearer/src/index.ts
+++ packages/create-bearer/src/index.ts
@@ -1,2 +1,2 @@
 #!/usr/bin/env node
-console.log('Hello from the Bearer Team! Learn more at https://www.bearer.sh/')
+console.log('Hello from the Bearer Team!\nLearn more at https://www.bearer.sh/ ')
