5f100922d0ab04d4e56b006a12834d4150de6c34
Tarik IHADJADENE <tarik.ihadjadene@gmail.com>
2019-02-27 14:14:23 +0100

    add the intent logger (#543)
    * add the intent logger
    
    * feedback
    
    * add jsdoc

--- packages/logger/src/logger.ts
+++ packages/logger/src/logger.ts
@@ -1,2 +1,22 @@
 import debug from 'debug'
 export default (scope: string) => debug(`bearer:${scope}`)
+
+export class BearerIntentLogger {
+  context: any
+  /**
+   * @param  {any} context
+   */
+  constructor(context: any) {
+    this.context = context
+  }
+  /**
+   * @param  {any} data
+   */
+  log = (data: any) => {
+    debug('bearer:intents')('%j', {
+      data,
+      scenarioId: this.context.integrationUuid,
+      intentName: this.context.intentName
+    })
+  }
+}
