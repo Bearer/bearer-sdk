ad39b1acba2126420d39752b57aa91fc2de9a830
Tanguy Antoine <tanguyantoine@users.noreply.github.com>
2019-07-08 11:23:54 +0200

    fix: simplify function bundling (#703)
    * fix: simplify function bundling
    
    - minify generated bundle
    - prevent duplication
    - speed up function build
    
    * fix: test

--- packages/cli/src/utils/function-ts-compiler-options.ts
+++ packages/cli/src/utils/function-ts-compiler-options.ts
@@ -1,12 +1,13 @@
-export default {
+import * as ts from 'typescript'
+const options: ts.CompilerOptions = {
   allowUnreachableCode: false,
   declaration: false,
-  lib: ['es2017'],
   noUnusedLocals: false,
   noUnusedParameters: false,
   allowSyntheticDefaultImports: true,
   experimentalDecorators: true,
-  moduleResolution: 'node',
-  module: 'es6',
-  target: 'es2017'
+  moduleResolution: ts.ModuleResolutionKind.NodeJs,
+  module: ts.ModuleKind.CommonJS,
+  target: ts.ScriptTarget.ES2017
 }
+export default options
