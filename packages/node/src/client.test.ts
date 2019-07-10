b85f3320e6e8749dcfa7bcf3c1e1882eabdc15f0
David Roe <dave.roe@bearer.sh>
2019-07-10 15:30:17 +0100

    Improvements to proxy support in node client (#708)
    * refactor: separate API for setting setup/auth ids
    
    Setup ids will become optional soon so let's make the API nicer to use.
    
    * feat: add ability to make request with dynamic method
    
    * fix: support client options in factory function

--- packages/node/src/client.test.ts
+++ packages/node/src/client.test.ts
@@ -2,11 +2,15 @@ import nock from 'nock'
 
 import clientFactory, { Bearer } from './client'
 const apiKey = 'spongeBobApiKey'
-
-const distantApi = jest.fn(() => ({ ok: 'ok' }))
+const okResponse = { ok: 'ok' }
+const distantApi = jest.fn(() => okResponse)
 
 describe('Bearer client', () => {
   const client = clientFactory(apiKey)
+
+  beforeEach(() => {
+    distantApi.mockClear()
+  })
 
   it('returns a client instance', () => {
     expect(client).toBeInstanceOf(Bearer)
@@ -23,7 +27,6 @@ You'll find you API key at this location: https://app.bearer.sh/keys`
 
   describe('#invoke', () => {
     it('send request to the function', async () => {
-      distantApi.mockClear()
       nock('https://int.bearer.sh', {
         reqheaders: {
           authorization: apiKey
@@ -35,52 +38,128 @@ You'll find you API key at this location: https://app.bearer.sh/keys`
       const { data } = await client.invoke('12345-integration-name', 'functionName')
 
       expect(distantApi).toHaveBeenCalled()
-      expect(data).toEqual({ ok: 'ok' })
+      expect(data).toEqual(okResponse)
     })
   })
 
   describe('#integration', () => {
     const integrationName = '12345'
     const api = client.integration(integrationName)
+    const query = { sponge: 'bob' }
+    const headers = {}
+    const body = '{"body":"data"}'
 
-    it('performs correct API calls', async () => {
-      distantApi.mockClear()
-      nock('https://int.bearer.sh', {
-        reqheaders: {
-          authorization: apiKey
-        }
-      })
-        .post(`/api/v4/functions/backend/${integrationName}/bearer-proxy/test`)
-        .query({ sponge: 'bob' })
-        .reply(200, distantApi)
+    interface IMockRequestParams {
+      method: string
+      extraHeaders?: Record<string, string>
+      body?: any
+    }
 
-      const { data } = await api.post('/test', { query: { sponge: 'bob' } })
-
-      expect(distantApi).toHaveBeenCalled()
-      expect(data).toEqual({ ok: 'ok' })
-    })
-
-    it('allows to make authenticated API calls', async () => {
-      const authId = 'abcde12345...'
-      distantApi.mockClear()
+    const mockRequest = ({ method, extraHeaders = {}, body }: IMockRequestParams) => {
       nock('https://int.bearer.sh', {
         reqheaders: {
           authorization: apiKey,
-          'Bearer-Auth-Id': authId
+          ...headers,
+          ...extraHeaders
         }
       })
-        .post(`/api/v4/functions/backend/${integrationName}/bearer-proxy/test`)
-        .query({ sponge: 'bob' })
+        .intercept(`/api/v4/functions/backend/${integrationName}/bearer-proxy/test`, method, body)
+        .once()
+        .query(query)
         .reply(200, distantApi)
+    }
 
-      const { data } = await api.auth({ authId }).post('/test', { query: { sponge: 'bob' } })
+    it('performs correct API calls', async () => {
+      mockRequest({ body, method: 'POST' })
+
+      const { data } = await api.post('/test', { headers, query, body })
 
       expect(distantApi).toHaveBeenCalled()
-      expect(data).toEqual({ ok: 'ok' })
+      expect(data).toEqual(okResponse)
     })
 
-    it('has an alias function "authenticate"', async () => {
-      expect(api.authenticate).toEqual(api.auth)
+    describe('#request', () => {
+      it('supports GET requests', async () => {
+        mockRequest({ method: 'GET' })
+
+        const { data } = await api.request('GET', '/test', { headers, query })
+
+        expect(distantApi).toHaveBeenCalled()
+        expect(data).toEqual(okResponse)
+      })
+
+      it('supports HEAD requests', async () => {
+        mockRequest({ method: 'HEAD' })
+
+        const { data } = await api.request('HEAD', '/test', { headers, query })
+
+        expect(distantApi).toHaveBeenCalled()
+        expect(data).toEqual(okResponse)
+      })
+
+      it('supports POST requests', async () => {
+        mockRequest({ body, method: 'POST' })
+
+        const { data } = await api.request('POST', '/test', { headers, query, body })
+
+        expect(distantApi).toHaveBeenCalled()
+        expect(data).toEqual(okResponse)
+      })
+
+      it('supports PUT requests', async () => {
+        mockRequest({ body, method: 'PUT' })
+
+        const { data } = await api.request('PUT', '/test', { headers, query, body })
+
+        expect(distantApi).toHaveBeenCalled()
+        expect(data).toEqual(okResponse)
+      })
+
+      it('supports PATCH requests', async () => {
+        mockRequest({ body, method: 'PATCH' })
+
+        const { data } = await api.request('PATCH', '/test', { headers, query, body })
+
+        expect(distantApi).toHaveBeenCalled()
+        expect(data).toEqual(okResponse)
+      })
+
+      it('supports DELETE requests', async () => {
+        mockRequest({ body, method: 'DELETE' })
+
+        const { data } = await api.request('DELETE', '/test', { headers, query, body })
+
+        expect(distantApi).toHaveBeenCalled()
+        expect(data).toEqual(okResponse)
+      })
+    })
+
+    describe('#auth', () => {
+      it('sends any configured auth id in a Bearer-Auth-Id header', async () => {
+        const authId = 'abcde12345...'
+        mockRequest({ method: 'POST', extraHeaders: { 'Bearer-Auth-Id': authId } })
+
+        const { data } = await api.auth(authId).post('/test', { query })
+
+        expect(distantApi).toHaveBeenCalled()
+        expect(data).toEqual(okResponse)
+      })
+
+      it('has an alias function "authenticate"', async () => {
+        expect(api.authenticate).toEqual(api.auth)
+      })
+    })
+
+    describe('#setup', () => {
+      it('sends any configured setup id in a Bearer-Setup-Id header', async () => {
+        const setupId = 'test-setup-id'
+        mockRequest({ method: 'GET', extraHeaders: { 'Bearer-Setup-Id': setupId } })
+
+        const { data } = await api.setup(setupId).get('/test', { query })
+
+        expect(distantApi).toHaveBeenCalled()
+        expect(data).toEqual(okResponse)
+      })
     })
   })
 })
--- packages/node/src/client.ts
+++ packages/node/src/client.ts
@@ -38,8 +38,12 @@ class BearerClient {
     readonly authId?: string
   ) {}
 
-  public auth = ({ setupId, authId }: { setupId?: string; authId?: string }) => {
-    return new BearerClient(this.integrationId, this.options, this.bearerApiKey, setupId, authId)
+  public auth = (authId: string) => {
+    return new BearerClient(this.integrationId, this.options, this.bearerApiKey, this.setupId, authId)
+  }
+
+  public setup = (setupId: string) => {
+    return new BearerClient(this.integrationId, this.options, this.bearerApiKey, setupId, this.authId)
   }
 
   public authenticate = this.auth // Alias
@@ -49,30 +53,30 @@ class BearerClient {
    */
 
   public get = (endpoint: string, parameters?: BearerRequestParameters, options?: any) => {
-    return this.makeRequest('GET', endpoint, parameters, options)
+    return this.request('GET', endpoint, parameters, options)
   }
 
   public head = (endpoint: string, parameters?: BearerRequestParameters, options?: any) => {
-    return this.makeRequest('HEAD', endpoint, parameters, options)
+    return this.request('HEAD', endpoint, parameters, options)
   }
 
   public post = (endpoint: string, parameters?: BearerRequestParameters, options?: any) => {
-    return this.makeRequest('POST', endpoint, parameters, options)
+    return this.request('POST', endpoint, parameters, options)
   }
 
   public put = (endpoint: string, parameters?: BearerRequestParameters, options?: any) => {
-    return this.makeRequest('PUT', endpoint, parameters, options)
+    return this.request('PUT', endpoint, parameters, options)
   }
 
   public delete = (endpoint: string, parameters?: BearerRequestParameters, options?: any) => {
-    return this.makeRequest('DELETE', endpoint, parameters, options)
+    return this.request('DELETE', endpoint, parameters, options)
   }
 
   public patch = (endpoint: string, parameters?: BearerRequestParameters, options?: any) => {
-    return this.makeRequest('PATCH', endpoint, parameters, options)
+    return this.request('PATCH', endpoint, parameters, options)
   }
 
-  protected makeRequest = (
+  public request = (
     method: BearerRequestMethod,
     endpoint: string,
     parameters?: BearerRequestParameters,
@@ -172,12 +176,12 @@ in the form "{ headers: { "Foo": "bar" }, body: "My body" }"`)
  * Exports
  */
 
-export default (apiKey: string | undefined): Bearer => {
+export default (apiKey: string | undefined, options?: BearerClientOptions): Bearer => {
   if (!apiKey) {
     throw new InvalidAPIKey(apiKey)
   }
 
-  return new Bearer(apiKey)
+  return new Bearer(apiKey, options)
 }
 
 export { Bearer }
